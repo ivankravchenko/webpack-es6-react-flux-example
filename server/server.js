c4eb1963a16ffcda119d450f9fa7a71738a924d3
Ivan Kravchenko <i@ivankravchenko.com>
2015-08-03 23:09:01 -0400

    cleanup

--- server/server.js
+++ server/server.js
@@ -105,17 +105,3 @@ server.listen(port);
 if (process.env.NODE_ENV === 'development') {
     console.log('server.js is listening on port ' + port);
 }
-
-
-// old stuff ----
-
-// const express = require("express");
-// const http = require("http");
-// const app = express();
-// app.use(express.static(__dirname + "/../public"));
-
-// const server = http.createServer(app);
-// const PORT = process.env.PORT || 3000;
-// server.listen(PORT, function() {
-//   console.log("Server is listening on port " + PORT)
-// });
